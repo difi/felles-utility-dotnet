60207893925a827e06d321d866157bc88ed04dcf
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-30 13:29:16 +0200

    Lagt til sharedassembly

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -8,5 +8,8 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
+using System.Reflection;
+
 [assembly: AssemblyVersion("0.1.*")]
 [assembly: AssemblyFileVersion("0.1.*")]
