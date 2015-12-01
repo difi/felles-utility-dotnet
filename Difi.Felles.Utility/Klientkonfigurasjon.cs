3ab77c1074f88fc61d95aa79b5cd51315b8ef742
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-12-01 12:26:06 +0100

    Fjerner unødvendinge og ukorrekte kommentarer i logging.

--- Difi.Felles.Utility/Klientkonfigurasjon.cs
+++ Difi.Felles.Utility/Klientkonfigurasjon.cs
@@ -14,38 +14,30 @@ namespace Difi.SikkerDigitalPost.Klient
         public AbstraktMiljø Miljø { get; set; }
 
         /// <summary>
-        /// Angir host som skal benyttes i forbindelse med bruk av proxy. Både ProxyHost og ProxyPort må spesifiseres for at en proxy skal benyttes. Denne verdien kan også overstyres i 
-        /// applikasjonens konfigurasjonsfil gjennom med appSettings verdi med nøkkelen 'SDP:ProxyHost'.
-        /// </summary>
+        /// Angir host som skal benyttes i forbindelse med bruk av proxy. Både ProxyHost og ProxyPort må spesifiseres for at en proxy skal benyttes. 
         public string ProxyHost { get; set; }
 
         /// <summary>
-        /// Angir schema ved bruk av proxy. Standardverdien er 'https'. Denne verdien kan også overstyres i 
-        /// applikasjonens konfigurasjonsfil gjennom med appSettings verdi med nøkkelen 'SDP:ProxyScheme'.
+        /// Angir schema ved bruk av proxy. Standardverdien er 'https'.
         /// </summary>
         public string ProxyScheme { get; set; }
 
         /// <summary>
-        /// Angir portnummeret som skal benyttes i forbindelse med bruk av proxy. Både ProxyHost og ProxyPort må spesifiseres for at en proxy skal benyttes. Denne verdien kan også overstyres i 
-        /// applikasjonens konfigurasjonsfil gjennom med appSettings verdi med nøkkelen 'SDP:ProxyPort'.
+        /// Angir portnummeret som skal benyttes i forbindelse med bruk av proxy. Både ProxyHost og ProxyPort må spesifiseres for at en proxy skal benyttes.
         /// </summary>
         public int ProxyPort { get; set; }
 
         /// <summary>
-        /// Angir timeout for komunikasjonen fra og til meldingsformindleren. Default tid er 30 sekunder. Denne verdien kan også overstyres i 
-        /// applikasjonens konfigurasjonsfil gjennom med appSettings verdi med nøkkelen 'SDP:TimeoutIMillisekunder'.
+        /// Angir timeout for komunikasjonen fra og til meldingsformindleren. Default tid er 30 sekunder.
         /// </summary>
         public int TimeoutIMillisekunder { get; set; }
 
         /// <summary>
         /// Eksponerer et grensesnitt for logging hvor brukere kan integrere sin egen loggefunksjonalitet eller en tredjepartsløsning som f.eks log4net. For bruk, angi en annonym funksjon med 
-        /// følgende parametre: severity, konversasjonsid, metode, melding. Som default benyttes trace logging med navn 'SikkerDigitalPost.Klient' som kan aktiveres i applikasjonens konfigurasjonsfil. 
+        /// følgende parametre: severity, konversasjonsid, metode, melding.  
         /// </summary>
         public Action<TraceEventType, Guid?, string, string> Logger { get; set; }
 
-        /// <summary>
-        /// Indikerer om proxy skal benyttes for oppkoblingen mot meldingsformidleren.
-        /// </summary>
         public bool BrukProxy
         {
             get
@@ -53,18 +45,11 @@ namespace Difi.SikkerDigitalPost.Klient
                 return !string.IsNullOrWhiteSpace(ProxyHost) && ProxyPort > 0;
             }
         }
-
-        /// <summary>
-        /// Setter om logging skal gjøres til fil for alle meldinger som går mellom Klientbibliotek og Meldingsformidler
-        /// </summary>
+        
         public bool LoggXmlTilFil { get; set; }
 
         public string StandardLoggSti { get; set; }
 
-        /// <summary>
-        /// Klientkonfigurasjon som brukes ved oppsett av <see cref="SikkerDigitalPostKlient"/>.  Brukes for å sette parametere
-        /// som proxy, timeout og URI til meldingsformidler.
-        /// </summary>
         public Klientkonfigurasjon(AbstraktMiljø miljø)
         {
             Miljø = miljø;
