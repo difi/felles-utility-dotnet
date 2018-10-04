fe0acdf0bd05c3584e320847c9f48604d178ded5
Tor Hovland <tor.egil.hovland@bekk.no>
2018-10-04 12:14:16 +0200

    Migrated Difi.Felles.Utility to .NET Standard 2.0.

--- Difi.Felles.Utility/Properties/AssemblyInfo.cs
+++ Difi.Felles.Utility/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Difi.Felles.Utility")]
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("Difi.Felles.Utility.Tester")]
